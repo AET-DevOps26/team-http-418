ecdbb8f394e262908942762c75a7a497d12547f2
daniel <ge64tap@mytum.de>
2026-05-05 21:06:32 +0200

    change folder structure and use gradle.kts

---
+++ server/src/test/java/tum/devops/http418/Http418ApplicationTests.java
@@ -0,0 +1,13 @@
+package tum.devops.http418;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Http418ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
