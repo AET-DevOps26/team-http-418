0117fd9eddb8b2436691cf68c4ff24a31438d04a
daniel <ge64tap@mytum.de>
2026-06-05 00:04:26 +0200

    scrape data from tumonline

--- scraper/tumonline_scraper.py
+++ scraper/tumonline_scraper.py
@@ -1,5 +1,6 @@
 import asyncio
 import itertools
+import re
 from xml.etree import ElementTree as ET
 
 import aiohttp
@@ -14,21 +15,44 @@ async def fetch_page(session: aiohttp.ClientSession, offset: int, semester_id: i
         async with session.get(url) as response:
             assert response.status == 200
             text = await response.text()
-            return ET.fromstring(text)
+            text = re.sub(r"[\x00-\x08\x0B\x0C\x0E-\x1F]", " ", text) #sanitize invalid xml characters
+        return ET.fromstring(text)
 
 
-async def fetch_lectures(semester_id: int, debug: bool) -> list[ET.Element]:
+async def fetch_details(session: aiohttp.ClientSession, course:ET.Element)->tuple[ET.Element,ET.Element]:
+    try:
+        lecture_id = course.find("id").text
+    except AttributeError as e:
+        raise AttributeError(f"lecture {course} has no id\n{e}") from e
+    async with semaphore:
+        async with session.get(f"{base_url}/{lecture_id}") as response:
+            assert response.status == 200
+            text = await response.text()
+            text = re.sub(r"[\x00-\x08\x0B\x0C\x0E-\x1F]", " ", text) #sanitize invalid xml characters
+            try:
+                resources = ET.fromstring(text).findall("resource")
+                assert len(resources) == 1, f"expected exactly one resource for {lecture_id}, got {len(resources)}"
+                return course, resources[0]
+            except Exception as e:
+                raise Exception(f"could not parse {lecture_id} with content:\n{text}") from e
+async def fetch_courses(semester_id: int, detailed: bool, debug: bool) -> list[tuple[ET.Element,ET.Element]]:
     stepsize = 20
     async with aiohttp.ClientSession() as session:
-        # get first page to get total number of lectures
+        # get first page to get total number of courses
         page = await session.get(f"{base_url}?$filter=termId-eq={semester_id}&$orderBy=title=ascnf&$skip=0")
         xml = ET.fromstring(await page.text())
         total = int(xml.find("totalCount").text)
-        print(f"expecting {total} lectures")
-        # fetch all lectures in parallel
-        if debug: total = 20 # only fetch 20 lectures for debugging
+        print(f"expecting {total} courses{', ignoring because debug is set' if debug else ''}")
+        # fetch all courses in parallel
+        if debug: total = 20 # only fetch 20 courses for debugging
         tasks = [fetch_page(session, off, semester_id, stepsize) for off in range(0, total, stepsize)]
-        lectures: list[ET.Element] = await asyncio.gather(*tasks)
-    flat = list(itertools.chain(*(map(lambda tree: tree.findall("courses"), lectures)))) #flatmap to course elements
-    assert len(flat) == total, f"only got {len(lectures)} lectures of {total}"
-    return flat
+        courses: list[ET.Element] = await asyncio.gather(*tasks)
+        flat = list(itertools.chain(*(map(lambda tree: tree.findall("courses"), courses)))) #flatmap to course elements
+        assert len(flat) == total, f"only got {len(courses)} courses of {total}"
+        print(f"fetching detailed data for {len(flat)} courses")
+
+        tasks = [fetch_details(session, course) for course in flat]
+        result: list[tuple[ET.Element, ET.Element]] = await asyncio.gather(*tasks)
+    return result
+
+
