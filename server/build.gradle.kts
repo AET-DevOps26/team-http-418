5a545acb14f22f9628dd0cf7f19473dceb0880f6
daniel <ge64tap@mytum.de>
2026-06-13 15:11:14 +0200

    automatic final assignments

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
 	id("org.springframework.boot") version "4.0.6"
 	id("io.spring.dependency-management") version "1.1.7"
 	id("com.diffplug.spotless") version "7.0.4"
+	id("org.openrewrite.rewrite") version "7.34.0"
 }
 
 group = "tum.devops"
@@ -23,6 +24,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	add("rewrite", "org.openrewrite.recipe:rewrite-static-analysis:2.36.0")
 }
 
 tasks.withType<Test> {
@@ -36,6 +38,10 @@ spotless {
 		trimTrailingWhitespace()
 		endWithNewline()
 	}
+}
+
+rewrite {
+	activeRecipe("org.openrewrite.staticanalysis.FinalizeLocalVariables")
 }
 
 tasks.named("check") {
