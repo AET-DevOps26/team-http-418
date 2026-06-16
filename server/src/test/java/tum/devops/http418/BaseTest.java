865ce0b176089fe149c64c517b2afe56724f3fd1
daniel <ge64tap@mytum.de>
2026-06-17 01:00:59 +0200

    fix pipline

--- server/src/test/java/tum/devops/http418/BaseTest.java
+++ server/src/test/java/tum/devops/http418/BaseTest.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.TestPropertySource;
 @Import(TestConfig.class)
 @TestPropertySource(properties = {"SPRING_DATASOURCE_URL=jdbc:h2:mem:testdb", "SPRING_DATASOURCE_USERNAME=sa",
 		"SPRING_DATASOURCE_PASSWORD=", "app.jwt.secret=change-me-change-me-change-me-change-me",
-		"app.jwt.access-token-ttl-seconds=3600", "app.refresh-token.ttl-seconds=604800"})
+		"app.jwt.access-token-ttl-seconds=3600", "app.refresh-token.ttl-seconds=604800", "API_VERSION=v1"})
 @ActiveProfiles("test")
 public class BaseTest {
 }
