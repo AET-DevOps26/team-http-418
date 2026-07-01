c99f50c0080b0d8b825a124654f8b7ba9b047b95
daniel <ge64tap@mytum.de>
2026-07-01 15:21:45 +0200

    correctly store referenced courses as json

--- services/server/src/main/java/tum/devops/http418/api/dto/ConversationMessageDTO.java
+++ services/server/src/main/java/tum/devops/http418/api/dto/ConversationMessageDTO.java
@@ -1,5 +1,7 @@
 package tum.devops.http418.api.dto;
 
-public record ConversationMessageDTO(String id, String role, String content, String referencedCourses,
-		String createdAt) {
+import java.util.List;
+
+public record ConversationMessageDTO(String id, String role, String content, List<SimpleCourseData> referencedCourses,
+									 String createdAt) {
 }
